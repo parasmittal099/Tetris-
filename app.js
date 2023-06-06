924b7a519211d06bada61a7ee434c3d8b0b4650d
parasmittal099 <parasmittal099@gmail.com>
2023-06-06 11:07:50 +0000

    making tetris game

--- app.js
+++ app.js
@@ -17,11 +17,38 @@ document.addEventListener('DOMContentLoaded',()=>{
         [1,width+1,width*2+1,width*2]
         [width,width*2,width*2+1,width*2+2]
     ]
-    const tTetromino = [
-
+    const zTetromino = [
+        [0,width,width+1,width*2+1],
+        [width+1,width+2,width*2,width*2+1],
+        [0,width,width+1,width*2+1],
+        [width+1,width+2,width*2,width*2+1]
     ]
+    const tTetromino = [
+        [1,width,width+1,width+2],
+        [1,width+1,width+2,width*2+1],
+        [width,width+1,width+2,width*2+1],
+        [1,width,width+1,width*2+1]
+    ]
+    const oTetromino = [
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1]
+    ]
+    const iTetromino=[
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3],
+        [1,width+2,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3],
+    ]
+    const theTetrominoes = [lTetromino,zTetromino,tTetromino,oTetromino,iTetromino];
+    let currentPosition = 4;
+    let current = theTetrominoes[0][0];
+     
+    function draw(){
+        
+    }
     
-    console.log(squares);
 })
 
 
