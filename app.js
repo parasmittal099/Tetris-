a0519afdc3c7ad019caeb39a44e3929743bb7def
parasmittal099 <parasmittal099@gmail.com>
2023-06-27 21:39:22 +0000

    1st commit

---
+++ app.js
@@ -0,0 +1,12 @@
+//Browser when it loads a webpage it creates a- 
+// Document Object Model (DOM)
+//from this JS can change all HTML elements , attributes 
+// JS can change CSS styles 
+// Can create new HTML events , react to them 
+
+//addEventListener() attaches an even handler 
+// to specified element and 
+//doesnt overrise existing event handlers 
+document.addEventListener('DOMContentLoaded',() => {
+    
+})
